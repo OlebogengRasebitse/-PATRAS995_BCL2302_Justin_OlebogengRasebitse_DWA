e5e1b9cce76fa85861599c942812c5edac34b97e
OlebogengRasebitse <patiencerasebitse@gmail.com>
2023-06-06 15:53:05 +0200

    Added call stack to second last user story

--- DWA5/script.js
+++ DWA5/script.js
@@ -26,32 +26,33 @@
 //   }
 // });
 
-const form = document.querySelector("[data-form]");
-const result = document.querySelector("[data-result]");
+// const form = document.querySelector("[data-form]");
+// const result = document.querySelector("[data-result]");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const entries = new FormData(event.target);
-  const { dividend, divider } = Object.fromEntries(entries);
+// form.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   const entries = new FormData(event.target);
+//   const { dividend, divider } = Object.fromEntries(entries);
 
-  if (dividend === "" || divider === "") {
-    result.innerText = "Division not performed. Both values are required in inputs. Try again";
-  } else if (isNaN(dividend) || isNaN(divider)) {
-    result.innerText = "Division not performed. Invalid number provided. Try again";
-    console.error("Invalid number provided");
-  } else if (dividend < 0 || divider < 0) {
-    result.innerText = "Division not performed. Invalid number provided. Try again";
-    console.error("Division by zero");
-  } else if (dividend === "YOLO" && divider === "+++") {
-    const errorMessage = "Something critical went wrong. Please reload the page";
-    document.documentElement.innerHTML = errorMessage;
-    console.error("Critical error occurred");
-    throw new Error(errorMessage);
-  } else {
-    const divisionResult = Math.floor(dividend / divider);
-    result.innerText = divisionResult;
-  }
-});
+//   if (dividend === "" || divider === "") {
+//     result.innerText = "Division not performed. Both values are required in inputs. Try again";
+//   } else if (isNaN(dividend) || isNaN(divider)) {
+//     result.innerText = "Division not performed. Invalid number provided. Try again";
+//     console.error("Invalid number provided");
+//   } else if (dividend < 0 || divider < 0) {
+//     result.innerText = "Division not performed. Invalid number provided. Try again";
+//     console.error("Division by zero");
+    
+//   } else if (dividend === "YOLO" && divider === "+++") {
+//     const errorMessage = "Something critical went wrong. Please reload the page";
+//     document.documentElement.innerHTML = errorMessage;
+//     console.error("Critical error occurred");
+//     throw new Error(errorMessage);
+//   } else {
+//     const divisionResult = Math.floor(dividend / divider);
+//     result.innerText = divisionResult;
+//   }
+// });
 
 
 // const form = document.querySelector("[data-form]");
@@ -83,5 +84,46 @@ form.addEventListener("submit", (event) => {
 //   }
 // });
 
+
+
+
+
+const form = document.querySelector("[data-form]");
+const result = document.querySelector("[data-result]");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const entries = new FormData(event.target);
+  const { dividend, divider } = Object.fromEntries(entries);
+
+  try { 
+    //YOLO * +++
+    if (isNaN(dividend) || isNaN(divider)) {
+      const errorMessage = "Something critical went wrong. Please reload the page";
+      document.documentElement.innerHTML = errorMessage;
+      console.error("Critical error occurred");
+      throw new Error(errorMessage);
+
+      //empty inputs
+    } else {
+      if (dividend === "" || divider === "") {
+        result.innerText = "Division not performed. Both values are required in inputs. Try again";
+
+        //20 * -3
+      } else if (divider < 0 || dividend < 0 ) {
+        result.innerText = "Division not performed. Invalid number provided. Try again";
+        throw new Error("Division not performed. Invalid number provided. Try again");
+
+        //20*-7
+      } else {
+        const divisionResult = Math.floor(dividend / divider);
+        result.innerText = divisionResult;
+      }
+    }
+  } catch (error) {
+    // Handle the error if necessary
+    console.error(error);
+  }
+});
 
 
