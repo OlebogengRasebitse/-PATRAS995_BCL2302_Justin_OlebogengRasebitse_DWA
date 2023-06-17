f95b3deee09f2edba4594ff9836ef9f205e8abff
OlebogengRasebitse <patiencerasebitse@gmail.com>
2023-06-17 15:44:07 +0200

    Copied Book Connect files to DWA9

---
+++ DWA9/theme.js
@@ -0,0 +1,13 @@
+export const bookQuerySelector = {
+    searchCancelBtn: document.querySelector('[data-search-cancel]'),
+    settingsCancelBtn: document.querySelector('[data-settings-cancel]'),
+    headerSearchBtn: document.querySelector('[data-header-search]'),
+    headerSettingsBtn: document.querySelector('[data-header-settings]'),
+    listCloseBtn: document.querySelector('[data-list-close]'),
+    listItems: document.querySelector('[data-list-items]'),
+    listButton: document.querySelector('[data-list-button]'),
+    settingsForm: document.querySelector('[data-settings-form]'),
+    searchForm: document.querySelector('[data-search-form]'),
+    newItems: document.querySelector('[data-list-items]')
+  };
+  
