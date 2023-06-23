5b8ddcb29c44908a4bf8f4dc144c18a1a0e48853
OlebogengRasebitse <patiencerasebitse@gmail.com>
2023-06-23 11:59:34 +0200

    Edited DWA11 code and created a store using Redux.

--- DWA11/script.js
+++ DWA11/script.js
@@ -1,28 +1,24 @@
-import { createStore } from 'redux';
-
-// Define the initial state of the store
 const initialState = {
   count: 0,
   subtractDisabled: true,
   addDisabled: false,
 };
 
-// Define the reducer function to handle actions and update the state
 function reducer(state = initialState, action) {
   switch (action.type) {
     case 'SUBTRACT':
-      const newSubtractDisabled = state.count - STEP_AMOUNT <= MIN_NUMBER;
+      const newSubtractDisabled = state.count - 1 <= 0;
       return {
         ...state,
-        count: Math.max(state.count - STEP_AMOUNT, MIN_NUMBER),
+        count: Math.max(state.count - 1, 0),
         subtractDisabled: newSubtractDisabled,
         addDisabled: false,
       };
     case 'ADD':
-      const newAddDisabled = state.count + STEP_AMOUNT >= MAX_NUMBER;
+      const newAddDisabled = state.count + 1 >= Infinity;
       return {
         ...state,
-        count: Math.min(state.count + STEP_AMOUNT, MAX_NUMBER),
+        count: Math.min(state.count + 1, Infinity),
         subtractDisabled: false,
         addDisabled: newAddDisabled,
       };
@@ -38,25 +34,37 @@ function reducer(state = initialState, action) {
   }
 }
 
-// Create the store with the reducer
-const store = createStore(reducer);
+const store = window.Redux.createStore(reducer);
 
-// Function to update the console output based on the store state
-function updateConsoleOutput() {
+function logState() {
   const state = store.getState();
   console.log(`Count: ${state.count}`);
-  console.log(`Subtract Disabled: ${state.subtractDisabled}`);
-  console.log(`Add Disabled: ${state.addDisabled}`);
 }
 
-// Dispatch actions to modify the state
+console.log("Scenario: Increment the counter by one");
+logState();
+
+console.log("\nGIVEN no interactions have been performed yet");
+console.log("WHEN an “ADD” action is dispatched");
 store.dispatch({ type: 'ADD' });
+
+console.log("AND another “ADD” action is dispatched");
 store.dispatch({ type: 'ADD' });
+
+logState(); // The state should show a count of 2
+
+console.log("\nGIVEN the current count in the state is 2");
+console.log("WHEN a “SUBTRACT” action is dispatched");
 store.dispatch({ type: 'SUBTRACT' });
+
+logState(); // The state should display a count of 1
+
+console.log("\nScenario: Resetting the Tally Counter");
+logState();
+
+console.log("\nGIVEN the current count in the state is 1");
+console.log("WHEN a “RESET” action is dispatched");
 store.dispatch({ type: 'RESET' });
 
-// Subscribe to the store to receive updates and update the console output accordingly
-store.subscribe(updateConsoleOutput);
+logState(); // The state should display a count of 0
 
-// Initial console output
-updateConsoleOutput();
