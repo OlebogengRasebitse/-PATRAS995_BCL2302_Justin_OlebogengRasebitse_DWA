139e57fd616d6baf6c7186ad79c971f4e60880d8
OlebogengRasebitse <patiencerasebitse@gmail.com>
2023-06-28 15:07:57 +0200

    formated document.

--- DWA13/script.js
+++ DWA13/script.js
@@ -3,45 +3,47 @@ const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikki
 
 // forEach works! It allows us to go through each item in an array and perform a specific action with it
 names.forEach((eachName) => {
-    console.log(eachName);
-  });
+  console.log(eachName);
+});
 
 //Name and province
-names.forEach((eachName,index) => {
-    console.log(`${eachName}, ${provinces[index]}`);
-  });
+names.forEach((eachName, index) => {
+  console.log(`${eachName}, ${provinces[index]}`);
+});
 
-  //map to upperCase
-  const uppercaseProvinces = provinces.map((province) => {
-    return province.toUpperCase();
-  });
-  console.log(uppercaseProvinces) 
+//map to upperCase
+const uppercaseProvinces = provinces.map((province) => {
+  return province.toUpperCase();
+});
+console.log(uppercaseProvinces)
 
-  //Names character number.
-  const nameLength = names.map((name) => {
-   return name.length;
-  });
-  console.log(nameLength);
+//Names character number.
+const nameLength = names.map((name) => {
+  return name.length;
+});
+console.log(nameLength);
 
-  //Using toSorted to sort all provinces alphabetically.
-  const sortedProvinces = provinces.sort();
-  console.log(sortedProvinces);
+//Using toSorted to sort all provinces alphabetically.
+const sortedProvinces = provinces.sort();
+console.log(sortedProvinces);
 
-  //Filtre out all provinces with the word Cape.
-  const filteredProvinces = provinces.filter((province) => {
-    return !province.includes('Cape');
-  });
-  console.log(filteredProvinces.length);
+//Filtre out all provinces with the word Cape.
+const filteredProvinces = provinces.filter((province) => {
+  return !province.includes('Cape');
+});
+console.log(filteredProvinces.length);
 
-  //boolean array by using map and some to determine whether a name contains an S character.
-  const containsSArray = names.map((name) => {
-    return name.includes('S');
-  });
-  console.log(containsSArray);
+//boolean array by using map and some to determine whether a name contains an S character.
+const containsSArray = names.map((name) => {
+  return name.includes('S');
+});
+console.log(containsSArray);
 
-  // Task 8: Use reduce to create an object indicating the province of each individual
+// Task 8: Use reduce to create an object indicating the province of each individual
 const individualsByProvince = names.reduce((result, name, index) => {
   result[name] = provinces[index];
   return result;
 }, {});
 console.log(individualsByProvince);
+
+
