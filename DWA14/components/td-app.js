de301c4d5ee6f833cdf625ec140b2dd93913dae7
OlebogengRasebitse <patiencerasebitse@gmail.com>
2023-06-30 13:47:25 +0200

    Applied lit.dev to code.

---
+++ DWA14/components/td-app.js
@@ -0,0 +1,9 @@
+import { html } from "../libs/lit-html";
+/**
+ * @returns {any}
+ */
+class app extends LitElement {
+    render() {
+        return html`<div>123</div>`;
+    }
+}
--- DWA14/script.js
+++ DWA14/script.js
@@ -1,57 +1,108 @@
-import {LitElement, html} from 'lit';
-
-class ta
+import { html, css, LitElement } from 'https://cdn.skypack.dev/lit';
 
 
 
-// class TallyApp extends LitElement {
-//   static styles = css`
-//     button {
-//       margin: 5px;
-//     }
-//   `;
 
-//   static properties = {
-//     count: { type: Number },
-//   };
+class TallyApp extends LitElement {
+  static styles = css`
+    :host {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          font-family: Arial, sans-serif;
+        }
 
-//   constructor() {
-//     super();
-//     this.count = 0;
-//     this.minValue = 0;
-//     this.maxValue = 10;
-//   }
+        .counter {
+          display: flex;
+          align-items: center;
+          margin-top: 16px;
+        }
 
-//   increment() {
-//     if (this.count < this.maxValue) {
-//       this.count++;
-//     }
-//   }
+        .counter button {
+          font-size: 24px;
+          padding: 8px;
+        }
 
-//   decrement() {
-//     if (this.count > this.minValue) {
-//       this.count--;
-//     }
-//   }
+        .counter span {
+          font-size: 36px;
+          margin: 0 16px;
+          color: blueviolet;
 
-//   render() {
-//     let stateMessage = '';
+        }
 
-//     if (this.count === this.minValue) {
-//       stateMessage = 'Minimum Reached';
-//     } else if (this.count === this.maxValue) {
-//       stateMessage = 'Maximum Reached';
-//     }
+        .limit-reached {
+          color: red;
+          font-size: 14px;
+          margin-top: 8px;
+        }
 
-//     return html`
-//       <div>
-//         <button @click=${this.decrement} ?disabled=${this.count === this.minValue}>-</button>
-//         <span>${this.count}</span>
-//         <button @click=${this.increment} ?disabled=${this.count === this.maxValue}>+</button>
-//       </div>
-//       <p>${stateMessage}</p>
-//     `;
-//   }
-// }
 
-// customElements.define('tally-app', TallyApp);
+        .counter__actions {
+  display: flex;
+ }
+ 
+ .in{
+     background: none;
+     width: 50%;
+     border-width: 0;
+     color: var(--color-white);
+     font-size: 3rem;
+     height: 10rem;
+     border-bottom: 1px solid var(--color-light-grey);
+     transition: transform 0.1s;
+     transform: translateY(0);
+   
+ }
+ 
+ .counter__button:active {
+     background: var(--color-medium-grey);
+     transform: translateY(2%);
+ }
+    
+  `;
+
+  static properties = {
+    count: { type: Number },
+  };
+
+  static constraints = {
+    MAX_COUNT: 5,
+    MIN_COUNT: -5,
+  };
+
+  constructor() {
+    super();
+    this.count = 0;
+  }
+
+  increment() {
+    if (this.count < this.constructor.constraints.MAX_COUNT) {
+      this.count++;
+    }
+  }
+
+  decrement() {
+    if (this.count > this.constructor.constraints.MIN_COUNT) {
+      this.count--;
+    }
+  }
+
+  render() {
+    return html`
+      <h1>Tally App</h1>
+      <div>
+        <button class=increament @click=${this.increment}>+</button>
+        <span>${this.count}</span>
+        <button @click=${this.decrement}>-</button>
+      </div>
+      ${this.count === this.constructor.constraints.MAX_COUNT
+            ? html`<p class="limit-reached">Maximum count reached</p>`
+            : ''}
+          ${this.count === this.constructor.constraints.MIN_COUNT
+            ? html`<p class="limit-reached">Minimum count reached</p>`
+            : ''}
+    `;
+  }
+}
+
+customElements.define('tally-app', TallyApp);
